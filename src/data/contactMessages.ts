4738f2560153af18142141459987fff9246d300e
DamianKaizhen <damian.k@kaizhenai.com>
2025-12-24 16:17:46 -0600

    New contact message from John Doe

--- src/data/contactMessages.ts
+++ src/data/contactMessages.ts
@@ -13,5 +13,18 @@ export const contactMessages: ContactMessage[] = [
     submittedAt: '2025-11-03T04:47:14.839Z',
     status: 'new',
     
+  },
+  {
+    id: 'msg_1766614666484_ah2uuyb',
+    name: 'John Doe',
+    email: 'test@email.com',
+    phone: '1234567890',
+    projectType: 'kitchen',
+    timeline: '1-2months',
+    message: 'Sample description for a kitchen project.',
+    attachments: ['https://owcahjzz8kidiuwp.public.blob.vercel-storage.com/1766614665011-Vicenza_Oak.png', 'https://owcahjzz8kidiuwp.public.blob.vercel-storage.com/1766614665864-groupimage.png', 'https://owcahjzz8kidiuwp.public.blob.vercel-storage.com/1766614666181-Potential%20Customer%2020251222%20-%20DarkGreyShiny.pdf'],
+    submittedAt: '2025-12-24T22:17:46.484Z',
+    status: 'new',
+    
   }
 ];
