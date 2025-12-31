446a81d946d46c32aa57f09baa19b7c73bd5725a
DamianKaizhen <damian.k@kaizhenai.com>
2025-12-31 16:58:13 -0600

    New visualizer submission from Damian

--- src/data/visualizerSubmissions.ts
+++ src/data/visualizerSubmissions.ts
@@ -10,8 +10,17 @@ export const visualizerSubmissions: VisualizerSubmission[] = [
     roomImage: 'https://owcahjzz8kidiuwp.public.blob.vercel-storage.com/1767220936319-Image_20250424145342.jpg',
     submittedAt: '2025-12-31T22:42:16.794Z',
     status: 'new',
-    // Legacy fields for backwards compatibility
-    finishId: 'f_mhjof2j18je',
-    finishName: 'First Class',
+    
+  },
+  {
+    id: 'vis_1767221892650_5iy7cjz',
+    name: 'Damian',
+    email: 'test@email.com',
+    phone: '123344443',
+    finishes: [{ id: 'f_mhjl7qjb22u', name: 'Acacia Honey', imageUrl: 'https://asearepsd4iqjdj2.public.blob.vercel-storage.com/1762201356612-Acacia_Honey.png' }, { id: 'f_mhjof2j18je', name: 'First Class', imageUrl: 'https://owcahjzz8kidiuwp.public.blob.vercel-storage.com/1762206739548-First_Class.png' }],
+    roomImage: 'https://owcahjzz8kidiuwp.public.blob.vercel-storage.com/1767221892198-Image_20250424145342.jpg',
+    submittedAt: '2025-12-31T22:58:12.651Z',
+    status: 'new',
+    
   }
 ];
