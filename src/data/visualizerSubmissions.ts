1731c2bffc7f0c3eb7408307908235847a68aca1
DamianKaizhen <damian.k@kaizhenai.com>
2025-12-31 17:04:12 -0600

    New visualizer submission from Damian

--- src/data/visualizerSubmissions.ts
+++ src/data/visualizerSubmissions.ts
@@ -22,5 +22,16 @@ export const visualizerSubmissions: VisualizerSubmission[] = [
     submittedAt: '2025-12-31T22:58:12.651Z',
     status: 'new',
     
+  },
+  {
+    id: 'vis_1767222251754_pae7v3r',
+    name: 'Damian',
+    email: 'Test@test.com',
+    phone: '111111111111',
+    finishes: [{ id: 'f_mhjl7qjb22u', name: 'Acacia Honey', imageUrl: 'https://asearepsd4iqjdj2.public.blob.vercel-storage.com/1762201356612-Acacia_Honey.png' }, { id: 'f_mhjof2j18je', name: 'First Class', imageUrl: 'https://owcahjzz8kidiuwp.public.blob.vercel-storage.com/1762206739548-First_Class.png' }],
+    roomImage: 'https://owcahjzz8kidiuwp.public.blob.vercel-storage.com/1767222251393-Image_20250424145342.jpg',
+    submittedAt: '2025-12-31T23:04:11.761Z',
+    status: 'new',
+    
   }
 ];
